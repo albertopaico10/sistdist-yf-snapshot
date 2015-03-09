f1e57aa6fd73cd97fb3185908c902f7a51331f7c
albertopaico10@gmail.com <albertopaico10@gmail.com@dac51611-69f2-2c71-7c74-4b4adc2d3614>
2015-03-09 04:21:39 +0000

    test for connect both project.

--- clinic_project/web-clinic/src/main/java/com/project/george/web/InicioController.java
+++ clinic_project/web-clinic/src/main/java/com/project/george/web/InicioController.java
@@ -2,16 +2,31 @@ package com.project.george.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.project.george.facade.business.ClinicApplicationBusiness;
+
 @Controller
 public class InicioController {
-
+	
+	@Autowired
+	public ClinicApplicationBusiness clinicApplicationBusiness;
+	
 	@RequestMapping("inicio.htm")
     public ModelAndView show(final HttpServletRequest request) {
 		System.out.println("inside inicio htm");
+		
+		try {
+			System.out.println("Ready...??");
+			String value=clinicApplicationBusiness.mensajeTest("Carlos");
+			System.out.println("**** "+value);
+		} catch (Exception e) {
+			System.out.println("Error : "+e.toString());
+		}
+		System.out.println("Fin");
 		return new ModelAndView("redirect:logueo.htm"); 
 	}
 }
