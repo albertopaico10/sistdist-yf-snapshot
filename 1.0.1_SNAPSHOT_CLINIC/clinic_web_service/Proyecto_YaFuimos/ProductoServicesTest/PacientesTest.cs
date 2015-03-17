f5eec37421d4c35a000e13fedbef967cca5d08de
diego lopez <diegolopezcdm@hotmail.com>
2015-03-16 21:33:46 -0500

    added junit

---
+++ 1.0.1_SNAPSHOT_CLINIC/clinic_web_service/Proyecto_YaFuimos/ProductoServicesTest/PacientesTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProductoServicesTest
+{
+    [TestClass]
+    public class PacientesTest
+    {
+      
+        /*[TestMethod]
+        public void crearPaciente()
+        {
+            try{
+                PacientesWS.PacientesClient proxy = new PacientesWS.PacientesClient();
+                PacientesWS.Paciente usuarionuevo = proxy.crearPaciente("juan",
+                    null, "46844382", new DateTime(), null, null, null,
+                    null, new DateTime(), new DateTime(), 0, 0, 0, null, null);
+                Assert.AreEqual("juan", usuarionuevo.Nombre);
+                Assert.AreEqual("46844382", usuarionuevo.Dni);
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+            }
+            
+        }
+
+        [TestMethod]
+        public void obtenerPaciente()
+        {
+            PacientesWS.PacientesClient proxy = new PacientesWS.PacientesClient();
+            PacientesWS.Paciente usuarionuevo = proxy.obtenerPaciente(3);
+            Assert.AreEqual("Juan Luis", usuarionuevo.Nombre);
+        }
+
+        [TestMethod]
+        public void modificarPaciente()
+        {
+            PacientesWS.PacientesClient proxy = new PacientesWS.PacientesClient();
+            PacientesWS.Paciente usuarionuevo = proxy.ModificarPaciente(8007,"juan d",
+                null, "46844382", new DateTime(), null, null, null,
+                null, new DateTime(), new DateTime(), 0, 0, 0, null, null);
+            Assert.AreEqual("juan d", usuarionuevo.Nombre);
+        }
+
+        [TestMethod]
+        public void eliminarPaciente()
+        {
+            PacientesWS.PacientesClient proxy = new PacientesWS.PacientesClient();
+            proxy.EliminarPaciente(7999);
+            PacientesWS.Paciente usuarionuevo = proxy.obtenerPaciente(7999);
+            Assert.IsNull(usuarionuevo);
+
+        }*/
+    }
+}
