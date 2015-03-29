103aa7da8a3551061c76c89d9c89a93de385dee0
César Quispe Carrión <cesar4gusto@gmail.com>
2015-03-29 07:54:41 -0500

    Dominio/Kardex-DetalleKardex

---
+++ 1.0.1_SNAPSHOT_CLINIC/clinic_web_service/Proyecto_YaFuimos/ProductoService/Dominio/Kardex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace ProductoService.Dominio
+{
+    [DataContract]
+    public class Kardex
+    {
+        [DataMember] 
+        public int Id { get; set; }
+        [DataMember]
+        public int IdProduct { get; set; }
+        [DataMember]
+        public double PriceKardex { get; set; }
+        [DataMember]
+        public int CountProduct { get; set; }
+        [DataMember]
+        public int Status { get; set; }
+        [DataMember]
+        public int TotalEntry { get; set; }
+        [DataMember]
+        public int TotalEgress { get; set; }
+        [DataMember]
+        public String Description { get; set; }
+        [DataMember]
+        public DateTime DateCreated { get; set; }
+        [DataMember]
+        public double PriceTotalProducto { get; set; }
+        [DataMember]
+        public int MyProperty { get; set; }
+        [DataMember]
+        public double PriceTotalSale { get; set; }
+        
+    }
+}
---
+++ 1.0.1_SNAPSHOT_CLINIC/clinic_web_service/Proyecto_YaFuimos/ProductoService/Persistencia/KardexDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductoService.Persistencia
+{
+    public class KardexDAO
+    {
+
+        public 
+
+    }
+}
